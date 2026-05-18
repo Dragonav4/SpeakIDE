a3e3f8d9b63d78a7fd8698481d78f41d4439aac5
Danyil Danilian <daniliandan100500@gmail.com>
2026-05-18 19:33:09 +0200

    fix: Add missing SpeakIdeConstants import in OpenAiWhisperProvider

--- src/main/kotlin/com/danilian/speakide/stt/openai/OpenAiWhisperProvider.kt
+++ src/main/kotlin/com/danilian/speakide/stt/openai/OpenAiWhisperProvider.kt
@@ -3,6 +3,7 @@ package com.danilian.speakide.stt.openai
 import com.danilian.speakide.audio.AudioData
 import com.danilian.speakide.audio.toWav
 import com.danilian.speakide.settings.SecureStorage
+import com.danilian.speakide.settings.SpeakIdeConstants
 import com.danilian.speakide.settings.SpeakIdeSettings
 import com.danilian.speakide.stt.SttProvider
 import com.danilian.speakide.stt.SttResult
